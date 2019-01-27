217da4b635bf1f6aa9cad47c26f88a04ea0aefcc
CantonioA <cantonio@vallegrande.edu.pe>
2019-01-27 16:14:31 -0500

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aplicacion';
+  nombre = 'Fernando';
+  apellido = 'Herrera';
 }
